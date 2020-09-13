3f71669434c822eac47368384baa52682d345fc2
Sayed Sadeed <sayed.sadeed@gmail.com>
2020-09-13 11:39:34 +1000

    updated port

--- config/config.go
+++ config/config.go
@@ -21,14 +21,14 @@ type Cdn struct {
 func NewConfiguration() *Configuration {
 	viper.AutomaticEnv()
 
-	viper.SetDefault("HOST", "localhost")
+	viper.SetDefault("HOST", "0.0.0.0")
 	viper.SetDefault("PORT", "4040")
 	viper.SetDefault("CDN_BASE_URL", "https://api.cloudinary.com/v1_1")
 	viper.SetDefault("CloudName", "sayze")
 
 	return &Configuration{&Http{
-		Host: viper.GetString("HTTP_HOST"),
-		Port: viper.GetString("HTTP_PORT"),
+		Host: viper.GetString("HOST"),
+		Port: viper.GetString("PORT"),
 	}, &Cdn{
 		BaseUrl:   viper.GetString("CDN_BASE_URL"),
 		ApiKey:    viper.GetString("API_KEY"),
